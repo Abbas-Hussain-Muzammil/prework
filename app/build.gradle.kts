98cd0f53462d3fd9b418e060e4228193f255b64e
Abbas Hussain Muzammil <a.lnu25@vikes.csuohio.edu>
2023-12-31 18:52:05 -0500

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.preworksandroid"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.preworksandroid"
--- app/src/main/java/com/example/preworksandroid/MainActivity.kt
+++ app/src/main/java/com/example/preworksandroid/MainActivity.kt
@@ -1,21 +1,34 @@
 package com.example.preworksandroid
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var btn: Button
+    private var currentImageIndex = 0
+    private val images = arrayOf(R.drawable.cat, R.drawable.cat2)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        imageView = findViewById(R.id.myImageView)
+        btn = findViewById(R.id.myButton)
 
-        val btn: Button = findViewById(R.id.myButton)
+        // Set initial image
+        imageView.setImageResource(images[currentImageIndex])
+
         btn.setOnClickListener {
-            // Do something here
-            Toast.makeText(this, "Hello, nice to meet you too!", Toast.LENGTH_SHORT).show()
+            // Increment index to show the next image
+            currentImageIndex = (currentImageIndex + 1) % images.size
+            imageView.setImageResource(images[currentImageIndex])
 
+            // Show a toast message
+            Toast.makeText(this, "Image changed", Toast.LENGTH_SHORT).show()
         }
     }
 }
